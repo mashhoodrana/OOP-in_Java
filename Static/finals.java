dded6127edd6934408754b1f079fe41285a13fcf
Mashhood Rana <themashhoodrana@gmail.com>
2024-02-16 20:46:51 +0500

    Learning

---
+++ Static/finals.java
@@ -0,0 +1,22 @@
+package Static;
+
+final class finalas {
+    public final void show() {
+        System.out.println("Hello!");
+    }
+}
+
+
+class one extends finalas {  // can not extend from a final class
+
+    public void show()   // can not override final method
+    {
+        System.out.println("hy");
+    }
+}
+
+public class finals {
+    public static void main(String[] args) {
+        
+    }
+}
